38631e875ab3a4556204aa1fdbffd1a4cba5aa3a
Rahmi Emma Natania <75345652+axantina@users.noreply.github.com>
2026-06-05 13:26:11 +0900

    Add example configuration header for receiver

---
+++ receiver/config.example.h
@@ -0,0 +1,9 @@
+#ifndef CONFIG_EXAMPLE_H
+#define CONFIG_EXAMPLE_H
+
+#define WIFI_SSID "YOUR_WIFI_SSID"
+#define WIFI_PASSWORD "YOUR_WIFI_PASSWORD"
+#define SERVER_URL "http://your-server/post-esp-data.php"
+#define API_KEY "YOUR_API_KEY"
+
+#endif
