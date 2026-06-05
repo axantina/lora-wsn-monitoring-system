649fc78bd9bcf5d0b4104bd38d43a3f930d2142c
Rahmi Emma Natania <75345652+axantina@users.noreply.github.com>
2026-06-05 13:26:48 +0900

    Add example config with placeholders for DB and API key
    Added example configuration parameters for database and API key.

--- server/config.example.php
+++ server/config.example.php
@@ -1,1 +1,8 @@
+<?php
+$servername = "localhost";
+$dbname = "YOUR_DATABASE_NAME";
+$username = "YOUR_DATABASE_USERNAME";
+$password = "YOUR_DATABASE_PASSWORD";
 
+$api_key_value = "YOUR_API_KEY";
+?>
